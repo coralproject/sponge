71c01ec1174c2cf79accf557763204866e7ee0e4
Gabriela Rodriguez <gabelula@gmail.com>
2015-10-14 09:19:12 -0700

    Adds dummy mains.

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+Package main
+
+Import source database into mongodb
+
+*/
+package main
+
+import "fmt"
+
+func main() {
+	// get config
+	// import everything
+	fmt.Println("Nada Para ver")
+}
