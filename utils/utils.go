c3f33ddcdd9e87edba1239b8c96dd65024a4d10f
Gabriela Rodriguez <gabelula@gmail.com>
2015-10-20 11:38:58 -0700

    Helpful commands and code principles.

---
+++ utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+import "github.com/coralproject/mod-data-import/models"
+
+// Data is a struct that has all the db rows and error field
+type Data struct {
+	//Rows     *sql.Rows // Move into appropiate structure because this has to work for API too (no database/sql)
+	Comments []models.Comment
+	Error    error
+}
