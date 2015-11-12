798fe15dbc832c1bdb0ed031eba7165523094925
Gabriela Rodriguez <gabelula@gmail.com>
2015-11-12 11:19:01 -0800

    Add error types to the source package.

--- source/mysql.go
+++ source/mysql.go
@@ -10,18 +10,15 @@ package source
 
 import (
 	"database/sql"
-	"log"
 	"strings"
 
-	configuration "github.com/coralproject/sponge/config"
 	"github.com/coralproject/sponge/models"
 	"github.com/coralproject/sponge/utils"
 
 	_ "github.com/go-sql-driver/mysql" // Check if this can be imported not blank. To Do.
 )
 
-// Global configuration variables that holds all the configuration from the config file
-var config = *configuration.New() // Reads the configuration file
+// Global configuration variables that holds the credentials for mysql
 var credential = config.GetCredential("mysql")
 
 /* Implementing the Sources */
@@ -52,11 +49,12 @@ func (m *MySQL) GetTables() map[string]string {
 func (m *MySQL) GetData(tableName string, modelName string) utils.Data {
 
 	var d utils.Data
+	d.Type = modelName
 
 	db, err := m.open()
 	if err != nil {
-		log.Fatal("Error when connecting to database. ", err)
 		d.Error = err
+		return d
 	}
 	defer m.close(db)
 
@@ -64,33 +62,32 @@ func (m *MySQL) GetData(tableName string, modelName string) utils.Data {
 
 	// Returns data into a map that is a json structure
 	d.Rows, err = runQuery(db, modelName, queryString)
-	d.Type = modelName
 	if err != nil {
-		log.Fatal("Error when quering the DB ", err)
+		d.Error = err
 	}
 
 	return d
 }
 
+// Run the query on the db
 func runQuery(db *sql.DB, model string, query string) ([]models.Model, error) {
 
 	var m models.Model
 	var ms []models.Model
 
 	// Creates a Model of the type model. A model struct.
-	m, errM := models.New(model)
-	if errM != nil {
-		log.Fatal("Error when trying to create a new model. ", errM)
+	m, err := models.New(model)
+	if err != nil {
+		return nil, modelError{model: model}
 	}
 
 	sd, err := db.Query(query)
 	if err != nil {
-		log.Fatal("Error when quering the DB ", err)
-		return nil, err
+		return nil, queryError{query: query}
 	}
 	defer sd.Close()
 
-	ms, err = m.ProcessData(sd)
+	ms, err = m.Transform(sd)
 
 	return ms, err
 }
@@ -105,20 +102,18 @@ func connection() string {
 // Open gives back a pointer to the DB
 func (m *MySQL) open() (*sql.DB, error) {
 
-	var err error
-	m.Database, err = sql.Open("mysql", m.Connection)
+	database, err := sql.Open("mysql", m.Connection)
 	if err != nil {
-		log.Fatal("Could not connect to MySQL database with ", m.Connection, err)
-		return nil, err
+		return nil, connectError{connection: m.Connection}
 	}
 
-	err = m.Database.Ping()
-	if err != nil {
-		log.Fatal("Could not connect to the database with ", m.Connection, err)
-		return nil, err
+	if err = database.Ping(); err != nil {
+		return nil, connectError{connection: m.Connection}
 	}
 
-	return m.Database, nil
+	m.Database = database
+
+	return database, nil
 }
 
 // Close closes the db
@@ -126,15 +121,16 @@ func (m MySQL) close(db *sql.DB) error {
 	return db.Close()
 }
 
-// Get returns data from the query to the db
-func (m MySQL) get(db *sql.DB, query string) *sql.Rows {
-
-	// LOOK INTO config.Strategy to see which is the strategy to follow
-	d, err := db.Query(query)
-	if err != nil {
-		log.Fatal("Error when quering the DB ", err)
-	}
-
-	// To Do: it needs to return DATA type
-	return d
-}
+// // Get returns data from the query to the db
+// func (m MySQL) get(db *sql.DB, query string) (*sql.Rows, error) {
+//
+// 	// LOOK INTO config.Strategy to see which is the strategy to follow
+// 	d, err := db.Query(query)
+// 	if err != nil {
+// 		log.Fatal("Error when quering the DB ", err)
+// 		return nil, queryError{query: query}
+// 	}
+//
+// 	// To Do: it needs to return DATA type
+// 	return d, nil
+// }
