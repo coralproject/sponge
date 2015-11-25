679c8cc9a121e16db60562648a9d7e8c3acccce2
Gabriela Rodriguez <gabelula@gmail.com>
2015-11-25 14:11:45 -0800

    Fiddler has a transform function now. This is the only thing it does.

--- fiddler/fiddler.go
+++ fiddler/fiddler.go
@@ -1,30 +1,35 @@
 /*
-Package fiddler implements a way to get data into our local database source.
-
-Possible local sources:
-* MongoDB
-* Service Layer Shelf
-
-Code Principles
-
-* Secure. For MongoDB look at security checklist: https://docs.mongodb.org/master/administration/security-checklist/
-* Intentional
+Package fiddler transform, through a strategy file, data from external source into our local coral schema.
 
 */
 package fiddler
 
 import (
+	"database/sql"
+	"log"
+
 	configuration "github.com/coralproject/sponge/config"
 	"github.com/coralproject/sponge/models"
 )
 
-////// Structures  //////
-
-// LocalDB is an interface to a local database source
-type LocalDB interface {
-	NewLocalDB() (*LocalDB, error)
-	Add(string, []models.Model) error // []models.Model] is the structure we are using to recieve the data to be added
-}
-
 // global variables related to configuration
 var config = *configuration.New() // Reads the configuration file
+
+// Transform from external source data into the coral schema
+func Transform(modelName string, data *sql.Rows) ([]models.Model, error) {
+	var dataCoral []models.Model
+
+	m, err := models.New(modelName)
+	if err != nil {
+		log.Printf(err.Error())
+		return nil, err
+	}
+
+	dataCoral, err = m.Transform(data, config.GetTables()[modelName])
+	if err != nil {
+		log.Printf(err.Error())
+		return nil, err
+	}
+
+	return dataCoral, nil
+}
