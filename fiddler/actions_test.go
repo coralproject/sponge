a2bb9ba23286ece346c182326895949a5e7aaaf4
Gabriela Rodriguez <gabelula@gmail.com>
2015-12-03 12:32:29 -0800

    Not use it for now. Needs further thought.

--- fiddler/actions_test.go
+++ fiddler/actions_test.go
@@ -1,46 +1,46 @@
-package models
+package fiddler
 
-import (
-	"database/sql"
-	"testing"
-
-	"github.com/coralproject/sponge/config"
-	"github.com/stretchr/testify/assert"
-)
-
-// Signature New(table string) models.Model
-func TestNewAction(t *testing.T) {
-
-	// The func we are testing
-	c, err := New("Action")
-
-	assert.Equal(t, Action{}, c, "They should be equal.")
-	assert.Nil(t, err, "It should build an action without errors.")
-}
-
-// Signature (a Action) Transform(sd *sql.Rows, table config.Table) ([]Model, error)
-func TestTransformAction(t *testing.T) {
-
-	a, err := New("Action")
-
-	sd := mockMysqlRowsActions()
-	table := config.Table{}
-
-	var actions []Model
-
-	// The func we are testing
-	actions, err = a.Transform(sd, table)
-
-	assert.Nil(t, err, "It should not return any error.")
-
-	assert.NotNil(t, actions, "It should return actions.")
-}
-
-// Something that implements sd *sql.Rows
-func mockMysqlRowsActions() *sql.Rows {
-
-	var sd *sql.Rows
-
-	// To Do: Needs to return a valid sql.Rows stubbed
-	return sd
-}
+// import (
+// 	"database/sql"
+// 	"testing"
+//
+// 	"github.com/coralproject/sponge/config"
+// 	"github.com/stretchr/testify/assert"
+// )
+//
+// // Signature New(table string) models.Model
+// func TestNewAction(t *testing.T) {
+//
+// 	// The func we are testing
+// 	c, err := New("Action")
+//
+// 	assert.Equal(t, Action{}, c, "They should be equal.")
+// 	assert.Nil(t, err, "It should build an action without errors.")
+// }
+//
+// // Signature (a Action) Transform(sd *sql.Rows, table config.Table) ([]Model, error)
+// func TestTransformAction(t *testing.T) {
+//
+// 	a, err := New("Action")
+//
+// 	sd := mockMysqlRowsActions()
+// 	table := config.Table{}
+//
+// 	var actions []Model
+//
+// 	// The func we are testing
+// 	actions, err = a.Transform(sd, table)
+//
+// 	assert.Nil(t, err, "It should not return any error.")
+//
+// 	assert.NotNil(t, actions, "It should return actions.")
+// }
+//
+// // Something that implements sd *sql.Rows
+// func mockMysqlRowsActions() *sql.Rows {
+//
+// 	var sd *sql.Rows
+//
+// 	// To Do: Needs to return a valid sql.Rows stubbed
+// 	return sd
+// }
--- fiddler/notes.go
+++ fiddler/notes.go
@@ -1,57 +1,64 @@
-package models
+package fiddler
 
-import (
-	"database/sql"
-	"fmt"
-	"time"
-
-	"github.com/coralproject/sponge/config"
-)
-
-//* NOTES */
-
-// Note is a note on a piece of content
-type Note struct {
-	ID          string `json:"userId" bson:"userId"`
-	CommentID   string
-	CommentNote string    `json:"body" bson:"body"`
-	CreateDate  time.Time `json:"createdate" bson:"createdate"`
-	UpdateDate  time.Time `json:"updatedate" bson:"updatedate"`
-}
-
-// Print only print information about the comment
-func (n Note) Print() {
-	fmt.Println("Note: ", n.ID, n.CommentNote)
-}
-
-// Transform get the data from sd
-func (n Note) Transform(sd *sql.Rows, table config.Table) ([]Model, error) {
-	var note Note
-	var notes []Model
-
-	var createDate string
-	var updateDate string
-
-	for sd.Next() {
-		err := sd.Scan(&note.ID, &note.CommentID, &note.CommentNote, &createDate, &updateDate)
-		if err != nil {
-			return nil, scanError{error: err}
-		}
-
-		note.CreateDate, _ = time.Parse("2006-01-02", createDate) // To Do: I need to see how to dinamically discover what is the dateTime layout
-		note.UpdateDate, _ = time.Parse("2006-01-02", updateDate)
-
-		n := len(notes)
-		if len(notes) == cap(notes) {
-			// Comments is full and we must expand
-			// Double the size and add 1
-			newNotes := make([]Model, len(notes), 2*len(notes)+1)
-			copy(newNotes, notes)
-			notes = newNotes
-		}
-		notes = notes[0 : n+1]
-		notes[n] = note
-	}
-
-	return notes, nil
-}
+//
+// import (
+// 	"database/sql"
+// 	"fmt"
+// 	"time"
+//
+// 	"github.com/coralproject/shelf/pkg/srv/comment"
+// 	configuration "github.com/coralproject/sponge/config"
+// )
+//
+// //* NOTES */
+//
+// // Note is embedding the comment package to extend it
+// type Note struct {
+// 	comment.Note
+// }
+//
+// // // Note is a note on a piece of content
+// // type Note struct {
+// // 	ID          string `json:"userId" bson:"userId"`
+// // 	CommentID   string
+// // 	CommentNote string    `json:"body" bson:"body"`
+// // 	CreateDate  time.Time `json:"createdate" bson:"createdate"`
+// // 	UpdateDate  time.Time `json:"updatedate" bson:"updatedate"`
+// // }
+//
+// // Print only print information about the comment
+// func (n Note) Print() {
+// 	fmt.Println("Note: ", n.ID, n.CommentNote)
+// }
+//
+// // Transform get the data from sd
+// func (n Note) Transform(sd *sql.Rows, table configuration.Table) ([]Transformer, error) {
+// 	var note Note
+// 	var notes []Transformer
+//
+// 	var createDate string
+// 	var updateDate string
+//
+// 	for sd.Next() {
+// 		err := sd.Scan(&note.ID, &note.CommentID, &note.CommentNote, &createDate, &updateDate)
+// 		if err != nil {
+// 			return nil, scanError{error: err}
+// 		}
+//
+// 		note.CreateDate, _ = time.Parse("2006-01-02", createDate) // To Do: I need to see how to dinamically discover what is the dateTime layout
+// 		note.UpdateDate, _ = time.Parse("2006-01-02", updateDate)
+//
+// 		n := len(notes)
+// 		if len(notes) == cap(notes) {
+// 			// Comments is full and we must expand
+// 			// Double the size and add 1
+// 			newNotes := make([]Transformer, len(notes), 2*len(notes)+1)
+// 			copy(newNotes, notes)
+// 			notes = newNotes
+// 		}
+// 		notes = notes[0 : n+1]
+// 		notes[n] = note
+// 	}
+//
+// 	return notes, nil
+// }
