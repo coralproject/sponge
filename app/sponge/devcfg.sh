cf60194158f7e614a80c14308e7e82db3a68a830
Gabriela Rodriguez <gabelula@gmail.com>
2015-12-18 10:18:19 -0800

    Setup env var for pillar client.

--- app/sponge/devcfg.sh
+++ app/sponge/devcfg.sh
@@ -4,3 +4,8 @@ export MONGO_DB=coral
 export MONGO_USER=gaba
 export MONGO_PASS=
 export LOGGING_LEVEL=1
+
+
+export USER_URL=http://localhost:8080/api/import/user
+export ASSET_URL=http://localhost:8080/api/import/asset
+export COMMENT_URL=http://localhost:8080/api/import/comment
