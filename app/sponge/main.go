7ba52449e69c8b6f16bac51227dd3cecd0e8adf2
Gabriela Rodriguez <gabelula@gmail.com>
2015-12-22 08:36:13 -0800

    Adds logging and flag.

--- app/sponge/main.go
+++ app/sponge/main.go
@@ -5,6 +5,7 @@ Import external source database into local source and transform it
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/ardanlabs/kit/cfg"
@@ -14,6 +15,8 @@ import (
 	"github.com/coralproject/sponge/pkg/fiddler"
 	"github.com/coralproject/sponge/pkg/source"
 )
+
+var limitFlag int
 
 func init() {
 	logLevel := func() int {
@@ -25,6 +28,8 @@ func init() {
 	}
 
 	log.Init(os.Stderr, logLevel)
+
+	flag.IntVar(&limitFlag, "count", 10, "Number of rows that we are going to import at a time")
 }
 
 func main() {
@@ -32,6 +37,7 @@ func main() {
 	log.Dev("startup", "main", "Start")
 
 	// Connect to external source
+	log.User("main", "main", "### Connecting to external database...")
 	mysql, err := source.New("mysql") // To Do. 1. Needs to ensure maximum rate limit is not reached
 	if err != nil {
 		log.Error("startup", "main", err, "Connect to external MySQL")
@@ -40,7 +46,6 @@ func main() {
 
 	//Get All the tables's names that we have in the strategy json file
 	tables, err := mysql.GetTables()
-
 	if err != nil {
 		log.Error("startup", "main", err, "Get external MySQL tables")
 		return
@@ -49,28 +54,35 @@ func main() {
 	for _, modelName := range tables {
 
 		// Get the data
-		log.User("main", "main", "### Getting data '%s' from external source.\n", modelName)
-		data, err := mysql.GetData(modelName)
+		log.User("main", "main", "### Reading data from table '%s'. \n", modelName)
+		data, err := mysql.GetData(modelName, limitFlag)
 		if err != nil {
 			log.Error("main", "main", err, "Get external MySQL data")
 			//continue
 		}
 
+		total := len(data)
+		countRow := 0
 		//Transform the data row by row
 		log.User("main", "main", "# Transforming data to the coral schema.\n")
+		log.User("main", "main", "# And importing %v documents.", len(data))
 		// Loop on all the data
 		for _, row := range data {
+			countRow = countRow + 1
 			newRow, err := fiddler.TransformRow(row, modelName)
 			if err != nil {
 				log.Error("main", "main", err, "Error when transforming the row %s.", row)
 			}
 
 			// send the row to pillar
+			log.User("main", "main", "# %v/%v documents completed.", countRow, total)
 			err = coral.AddRow(newRow, modelName)
 			if err != nil {
 				log.Error("main", "main", err, "Error when adding the row %s.", row)
 			}
 		}
 	}
+	log.User("main", "main", "### Complete on %v seconds")
+
 	log.Dev("shutdown", "main", "Complete")
 }
