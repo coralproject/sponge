08ce47405eb5752cc0fdda414d54cf4a246c08c0
Gabriela Rodriguez <gabelula@gmail.com>
2016-01-05 14:29:49 -0800

    change test package name.

--- app/sponge/main_test.go
+++ app/sponge/main_test.go
@@ -1,4 +1,4 @@
-package tests
+package main
 
 import (
 	"testing"
@@ -6,23 +6,5 @@ import (
 	"github.com/ardanlabs/kit/tests"
 )
 
-func init() {
-	tests.Init("SPONGE")
-}
 
-//==============================================================================
 
-// TestMain helps to clean up the test data.
-func TestMain(m *testing.M) {
-	// db := db.NewMGO()
-	// defer db.CloseMGO()
-	//
-	// query.GenerateTestData(db)
-	// defer query.DropTestData()
-	//
-	// loadQuery(db, "basic.json")
-	// loadQuery(db, "basic_var.json")
-	// defer query.RemoveTestSets(db)
-	//
-	// m.Run()
-}
