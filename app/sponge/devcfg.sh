6a4b918061d6fb52122c0438545b06518953a816
Gabriela Rodriguez <gabelula@gmail.com>
2016-01-25 16:58:24 -0800

    Set env variables needed.

--- app/sponge/devcfg.sh
+++ app/sponge/devcfg.sh
@@ -1,11 +1,3 @@
-export MONGO_HOST=127.0.0.1:27017
-export MONGO_AUTHDB=gaba
-export MONGO_DB=coral
-export MONGO_USER=gaba
-export MONGO_PASS=
-export LOGGING_LEVEL=1
-
-
-export USER_URL=http://localhost:8080/api/import/user
-export ASSET_URL=http://localhost:8080/api/import/asset
-export COMMENT_URL=http://localhost:8080/api/import/comment
+export STRATEGY_CONF =
+export CREATE_INDEX =
+export LOGGING_LEVEL=log.DEV
