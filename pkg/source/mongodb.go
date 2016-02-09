317d414a863eaa361f10ecb2e37a2ff5692389db
Gabriela Rodriguez <gabelula@gmail.com>
2016-02-08 16:26:46 -0800

    Adds normalization function to get data from mongodb into a format that fiddler can process.

--- pkg/source/mongodb.go
+++ pkg/source/mongodb.go
@@ -4,6 +4,9 @@ Package source implements a way to get data from external MongoDB sources.
 package source
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/ardanlabs/kit/log"
 	"gopkg.in/mgo.v2"
 )
@@ -28,13 +31,13 @@ func (m MongoDB) GetTables() ([]string, error) {
 }
 
 // GetData returns the raw data from the tableName
-func (m MongoDB) GetData(coralTableName string, limit int, offset int, orderby string) ([]map[string]interface{}, error) { //(*sql.Rows, error) {
+func (m MongoDB) GetData(coralTableName string, offset int, limit int, orderby string) ([]map[string]interface{}, error) { //(*sql.Rows, error) {
 
 	var dat []map[string]interface{}
 
 	// Get the corresponding table to the modelName
 	collectionName := strategy.GetTableForeignName(coralTableName)
-	// tableFields := config.GetTableFields(modelName) // map[string]string
+	fields := strategy.GetTableForeignFields(coralTableName) //[]]map[string]string
 
 	// open a connection
 	session, err := m.initSession()
@@ -58,13 +61,26 @@ func (m MongoDB) GetData(coralTableName string, limit int, offset int, orderby s
 	db := session.DB(credential.Database)
 	col := db.C(collectionName)
 
-	err = col.Find(&dat).All(&dat)
+	//Get all the fields that we are going to get from the document { field: 1}
+	query := make(map[string]bool)
+	for _, f := range fields {
+		query[f["foreign"]] = true
+	}
+
+	//.Select(query)
+	err = col.Find(nil).Limit(limit).All(&dat) // TO DO: CHECK TO SEE IF THERE IS ANY OTHER WAY TO GET THIS
 	if err != nil {
 		log.Error("Importing", "GetData", err, "Get collection")
 		return nil, err
 	}
 
-	return dat, nil
+	datsm, err := normalizeData(dat)
+	if err != nil {
+		log.Error("Importing", "GetData", err, "Converting into simple structure")
+		return nil, err
+	}
+
+	return datsm, nil
 }
 
 // GetQueryData needs to be implemented for mongodb to implement the sourcer interface
@@ -97,4 +113,125 @@ func (m *MongoDB) initSession() (*mgo.Session, error) {
 func (m MongoDB) closeSession(session *mgo.Session) error {
 	session.Close()
 	return nil
+}
+
+// it prepares the data to have the transformations in fiddler
+// normalize converts into a map[string]string with the key a breadcrumb to the leaf, and the value being the leaf itself
+func normalizeData(mongoData []map[string]interface{}) ([]map[string]interface{}, error) {
+	var dat []map[string]interface{}
+
+	for _, j := range mongoData { // this is a slice of maps
+		d, e := normalizeDocument(j)
+		if e != nil {
+			return nil, e
+		}
+		// add d to dat
+		dat = append(dat, d)
+	}
+
+	return dat, nil
+}
+
+func normalizeDocument(document map[string]interface{}) (map[string]interface{}, error) {
+	d := make(map[string]interface{})
+
+	for i, k := range document { // this is the actual map
+		//fmt.Printf("## Index %s, normalizing %v \n\n", i, k)
+		m := normalize(i, k) // gets the id being the breadcrumb and val the leaf
+		//fmt.Printf("- Got %v\n\n\n", m)
+		for r, p := range m {
+			d[r] = p
+		}
+	}
+
+	return d, nil
+}
+
+// i is the first key. k could be a string or a map
+// it returns key and value
+func normalize(i string, k interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	switch v := k.(type) {
+	case map[string]interface{}: // if k is a map then go deeper
+		//fmt.Printf("** %v is map[string]interface{}\n\n", k)
+		for p, e := range k.(map[string]interface{}) {
+			switch v := e.(type) {
+			case map[string]interface{}:
+				//fmt.Printf("* %v is map[string]interface{}\n\n", e)
+				newi := strings.Join([]string{i, p}, ".")
+				newresult := normalize(newi, e)
+				// copy newresult into result
+				for v1, v2 := range newresult {
+					result[v1] = v2
+				}
+			case []map[string]interface{}:
+				//fmt.Printf("* %v is []map[string]interface{}\n\n", e)
+				for u, c := range v {
+					newi := strings.Join([]string{i, p, strconv.Itoa(u)}, ".")
+					newresult := normalize(newi, c)
+					for v1, v2 := range newresult {
+						result[v1] = v2
+					}
+				}
+			case map[string]string:
+				//fmt.Printf("* %v is map[string]string\n\n", e)
+				for a, b := range v {
+					newi := strings.Join([]string{i, p, a}, ".")
+					result[newi] = b
+				}
+			case []map[string]string:
+				//fmt.Printf("* %v is []map[string]string\n\n", e)
+				for u, c := range v {
+					newi := strings.Join([]string{i, p, strconv.Itoa(u)}, ".")
+					newresult := normalize(newi, c)
+					for v1, v2 := range newresult {
+						result[v1] = v2
+					}
+				}
+			case []interface{}:
+				//fmt.Printf("* %v is []interface{}\n\n", e)
+				for d1, d2 := range v {
+					newi := strings.Join([]string{i, p, strconv.Itoa(d1)}, ".")
+					newresult := normalize(newi, d2)
+					for v1, v2 := range newresult {
+						result[v1] = v2
+					}
+				}
+			default:
+				// fmt.Printf("* %v is no idea\n\n", e)
+				// fmt.Println(reflect.TypeOf(v))
+				newi := strings.Join([]string{i, p}, ".")
+				result[newi] = e
+			}
+		}
+	case map[string]string:
+		//fmt.Printf("** %v is map[string]string\n\n", k)
+		for p, e := range v {
+			newi := strings.Join([]string{i, p}, ".")
+			result[newi] = e
+		}
+	case []map[string]string:
+		//fmt.Printf("** %v is []map[string]string\n\n", k)
+		for u, c := range v {
+			newi := strings.Join([]string{i, strconv.Itoa(u)}, ".")
+			newresult := normalize(newi, c)
+			for v1, v2 := range newresult {
+				result[v1] = v2
+			}
+		}
+	case []map[string]interface{}:
+		for u, c := range v {
+			newi := strings.Join([]string{i, strconv.Itoa(u)}, ".")
+			newresult := normalize(newi, c)
+			for v1, v2 := range newresult {
+				result[v1] = v2
+			}
+		}
+	default: // if k is not a map then just return it as a string
+		//fmt.Printf("** %v is no idea\n\n", k)
+		result[i] = k
+	}
+
+	return result
 }
--- pkg/source/mongodb_test.go
+++ pkg/source/mongodb_test.go
@@ -2,6 +2,8 @@ package source
 
 import "testing"
 
+import "gopkg.in/mgo.v2/bson"
+
 func TestMongoGetData(t *testing.T) {
 	setupMongo()
 
@@ -44,4 +46,243 @@ func TestMongoGetTables(t *testing.T) {
 	}
 
 	teardown()
+}
+
+// Signature func normalize(i string, k interface{}) (string, string) {
+func TestNormalize(t *testing.T) {
+
+	// when k is a map
+	i := "firstkey"
+	k1 := map[string]string{"secondkey1": "value1", "secondkey2": "value2"}
+
+	m := normalize(i, k1)
+
+	if m["firstkey.secondkey1"] != "value1" {
+		t.Error("Expected different map")
+	}
+	if m["firstkey.secondkey2"] != "value2" {
+		t.Error("Expected different map")
+	}
+
+	// when k is a leaf
+	i = "firstkey"
+	k2 := "value"
+
+	m = normalize(i, k2)
+
+	if m[i] != k2 {
+		t.Error("Expected different map")
+	}
+
+	// when k is more complex
+	i = "firstkey"
+	n := map[string]string{
+		"thirdkey1": "value21",
+		"thirdkey2": "value22",
+	}
+	k3 := map[string]interface{}{
+		"secondkey1": "value1",
+		"secondkey2": n,
+	}
+
+	m = normalize(i, k3)
+
+	if m["firstkey.secondkey1"] != "value1" {
+		t.Error("Expected different map")
+	}
+
+	if m["firstkey.secondkey2.thirdkey1"] != "value21" {
+		t.Error("Expected different map")
+	}
+
+	if m["firstkey.secondkey2.thirdkey2"] != "value22" {
+		t.Error("Expected different map")
+	}
+}
+
+// Signature func (m MongoDB) normalize(mongoData []map[string]interface{}) ([]map[string]interface{}, error) {
+func TestNormalizeDocument(t *testing.T) {
+
+	// Simple map[string]strings
+	k1 := map[string]interface{}{"a": "1", "b": "2"}
+
+	m, e := normalizeDocument(k1)
+	if e != nil {
+		t.Errorf("Expected no error, got %v", e)
+	}
+
+	if m["a"] != "1" {
+		t.Error("Expected different map")
+	}
+
+	if len(m) != 2 {
+		t.Errorf("Expected 2, got %d", len(m))
+	}
+
+	k2 := map[string]interface{}{"a": "1", "b": map[string]string{"c": "3", "d": "4"}}
+
+	m, e = normalizeDocument(k2)
+	if e != nil {
+		t.Errorf("Expected no error, got %v", e)
+	}
+
+	if m["a"] != "1" {
+		t.Error("Expected different map")
+	}
+
+	if m["b.c"] != "3" {
+		t.Error("Expected different map")
+	}
+
+	if len(m) != 3 {
+		t.Errorf("Expected 3, got %d", len(m))
+	}
+
+	k3 := map[string]interface{}{"a": map[string]string{"e": "5", "f": "6"}, "b": map[string]string{"c": "3", "d": "4"}}
+
+	m, e = normalizeDocument(k3)
+	if e != nil {
+		t.Errorf("Expected no error, got %v", e)
+	}
+
+	if m["a.f"] != "6" {
+		t.Error("Expected different map")
+	}
+
+	if len(m) != 4 {
+		t.Errorf("Expected 4, got %d", len(m))
+	}
+
+	k4 := map[string]interface{}{"a": map[string]interface{}{"e": "5", "f": map[string]string{"g": "7", "h": "8"}}, "b": map[string]string{"c": "3", "d": "4"}}
+
+	m, e = normalizeDocument(k4)
+	if e != nil {
+		t.Errorf("Expected no error, got %v", e)
+	}
+
+	if m["a.f.g"] != "7" {
+		t.Error("Expected different map")
+	}
+
+	if m["b.c"] != "3" {
+		t.Error("Expected different map")
+	}
+
+	if m["b.d"] != "4" {
+		t.Error("Expected different map")
+	}
+
+	if len(m) != 5 {
+		t.Errorf("Expected 5, got %d", len(m))
+	}
+
+	k5 := map[string]interface{}{
+		"_id": bson.ObjectIdHex("556ba089d710290036ef099d"),
+		"object": map[string]interface{}{
+			"context": []map[string]interface{}{{
+				"uri": "http://washingtonpost.com/opinions/reformers-want-to-erase-confuciuss-influence-in-asia-thats-a-mistake/2015/05/28/529c1d3a-042e-11e5-a428-c984eb077d4e_story.html",
+			}},
+			"published": "2015-06-01T00:00:09Z",
+		},
+	}
+
+	m, e = normalizeDocument(k5)
+	if e != nil {
+		t.Errorf("Expected no error, got %v", e)
+	}
+
+	if m["_id"] != bson.ObjectIdHex("556ba089d710290036ef099d") {
+		t.Errorf("Expected other map.")
+	}
+
+	if m["object.context.0.uri"] != "http://washingtonpost.com/opinions/reformers-want-to-erase-confuciuss-influence-in-asia-thats-a-mistake/2015/05/28/529c1d3a-042e-11e5-a428-c984eb077d4e_story.html" {
+		t.Errorf("Expected other map.")
+	}
+
+	if m["object.published"] != "2015-06-01T00:00:09Z" {
+		t.Errorf("Expected other map.")
+	}
+
+	k6 := map[string]interface{}{
+		"_id": bson.ObjectIdHex("556ba08cd710290035cf6c74"),
+		"object": map[string]interface{}{
+			"context": []map[string]interface{}{{
+				"uri": "http://washingtonpost.com/news/to-your-health/wp/2015/05/31/no-stranger-to-brutal-sports-injuries-kerry-faces-a-long-road-to-recovery/",
+			}},
+			"published": "2015-06-01T00:00:12Z",
+		},
+	}
+
+	m, e = normalizeDocument(k6)
+	if e != nil {
+		t.Errorf("Expected no error, got %v", e)
+	}
+
+	if m["_id"] != bson.ObjectIdHex("556ba08cd710290035cf6c74") {
+		t.Errorf("Expected other map.")
+	}
+	if m["object.context.0.uri"] != "http://washingtonpost.com/news/to-your-health/wp/2015/05/31/no-stranger-to-brutal-sports-injuries-kerry-faces-a-long-road-to-recovery/" {
+		t.Errorf("Expected http://washingtonpost.com/news/to-your-health/wp/2015/05/31/no-stranger-to-brutal-sports-injuries-kerry-faces-a-long-road-to-recovery/. Got %v.", m["object.context.uri"])
+	}
+
+	if m["object.published"] != "2015-06-01T00:00:12Z" {
+		t.Errorf("Expected other map.")
+	}
+
+}
+
+// Signature: func (m MongoDB) normalizeData(mongoData []map[string]interface{}) ([]map[string]interface{}, error)
+func TestNormalizeData(t *testing.T) {
+
+	var k []map[string]interface{}
+
+	k = []map[string]interface{}{
+		map[string]interface{}{
+			"_id": bson.ObjectIdHex("556ba089d710290036ef099d"),
+			"object": map[string]interface{}{
+				"context": []map[string]interface{}{{
+					"uri": "http://washingtonpost.com/opinions/reformers-want-to-erase-confuciuss-influence-in-asia-thats-a-mistake/2015/05/28/529c1d3a-042e-11e5-a428-c984eb077d4e_story.html",
+				}},
+				"published": "2015-06-01T00:00:09Z",
+			},
+		},
+		map[string]interface{}{
+			"_id": bson.ObjectIdHex("556ba08cd710290035cf6c74"),
+			"object": map[string]interface{}{
+				"context": []map[string]interface{}{{
+					"uri": "http://washingtonpost.com/news/to-your-health/wp/2015/05/31/no-stranger-to-brutal-sports-injuries-kerry-faces-a-long-road-to-recovery/",
+				}},
+				"published": "2015-06-01T00:00:12Z",
+			},
+		},
+	}
+
+	m, e := normalizeData(k)
+	if e != nil {
+		t.Errorf("Expected not error, got %v", e)
+	}
+
+	if m[0]["_id"] != bson.ObjectIdHex("556ba089d710290036ef099d") {
+		t.Errorf("Expected other map.")
+	}
+
+	if m[0]["object.context.0.uri"] != "http://washingtonpost.com/opinions/reformers-want-to-erase-confuciuss-influence-in-asia-thats-a-mistake/2015/05/28/529c1d3a-042e-11e5-a428-c984eb077d4e_story.html" {
+		t.Errorf("Expected other map.")
+	}
+
+	if m[0]["object.published"] != "2015-06-01T00:00:09Z" {
+		t.Errorf("Expected other map.")
+	}
+
+	if m[1]["_id"] != bson.ObjectIdHex("556ba08cd710290035cf6c74") {
+		t.Errorf("Expected other map.")
+	}
+	if m[1]["object.context.0.uri"] != "http://washingtonpost.com/news/to-your-health/wp/2015/05/31/no-stranger-to-brutal-sports-injuries-kerry-faces-a-long-road-to-recovery/" {
+		t.Errorf("Expected http://washingtonpost.com/news/to-your-health/wp/2015/05/31/no-stranger-to-brutal-sports-injuries-kerry-faces-a-long-road-to-recovery/. Got %v.", m[1]["object.context.uri"])
+	}
+
+	if m[1]["object.published"] != "2015-06-01T00:00:12Z" {
+		t.Errorf("Expected other map.")
+	}
+
 }
