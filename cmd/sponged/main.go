aef90e2326c0b442356081327451a51049993162
David Erwin <davidjerwin@gmail.com>
2016-07-05 10:36:18 -0400

    Add sponged api server
    Initial commit for the sponged api server

---
+++ cmd/sponged/main.go
@@ -0,0 +1,40 @@
+// Sponged is the write layer for Coral ecosystems
+package main
+
+import (
+	"runtime"
+
+	"github.com/ardanlabs/kit/log"
+	"github.com/ardanlabs/kit/web/app"
+
+	"github.com/coralproject/sponge/cmd/sponged/handlers"
+	"github.com/coralproject/sponge/cmd/sponged/routes"
+)
+
+// These are set by the makefile with:
+// go build -ldflags "-X main.GitVersion=8e830ff -X main.GitRevision=123123123123 -X main.BuildDate=2016-01-25"
+var (
+	GitRevision = "<unknown>"
+	GitVersion  = "<unknown>"
+	BuildDate   = "<unknown>"
+	IntVersion  = "201605091500"
+)
+
+func main() {
+	log.User("startup", "Init", "Revision     : %q", GitRevision)
+	log.User("startup", "Init", "Version      : %q", GitVersion)
+	log.User("startup", "Init", "Build Date   : %q", BuildDate)
+	log.User("startup", "Init", "Int Version  : %q", IntVersion)
+	log.User("startup", "Init", "Go Version   : %q", runtime.Version())
+	log.User("startup", "Init", "Go Compiler  : %q", runtime.Compiler)
+	log.User("startup", "Init", "Go ARCH      : %q", runtime.GOARCH)
+	log.User("startup", "Init", "Go OS        : %q", runtime.GOOS)
+
+	handlers.Version.GitRevision = GitRevision
+	handlers.Version.GitVersion = GitVersion
+	handlers.Version.BuildDate = BuildDate
+	handlers.Version.IntVersion = IntVersion
+
+	app.Run(":3141", routes.API())
+
+}
